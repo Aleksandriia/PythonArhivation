a87afc52d314815b391455435284bf97d58c91ba
Alexandria <77101492+Aleksandriia@users.noreply.github.com>
2022-10-26 01:50:09 +0300

    Update file 2

--- arhivation_one_file.py
+++ arhivation_one_file.py
@@ -3,7 +3,7 @@
 
 import zipfile
 
-jungle_zip = zipfile.ZipFile('C:\\Users\\1\\Desktop\\file.zip', 'w')
-jungle_zip.write('C:\\Users\\1\\Desktop\\file', compress_type=zipfile.ZIP_DEFLATED)
+jungle_zip = zipfile.ZipFile('C:\\...\\Desktop\\file.zip', 'w')
+jungle_zip.write('C:\\...\\Desktop\\file', compress_type=zipfile.ZIP_DEFLATED)
 
 jungle_zip.close()
