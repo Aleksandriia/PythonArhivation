e7d70fad2b566518dc2b2cfd6ed751debd2a6521
Alexandria <77101492+Aleksandriia@users.noreply.github.com>
2022-10-26 01:49:20 +0300

    Update File

--- arhivator_many_files.py
+++ arhivator_many_files.py
@@ -1,11 +1,12 @@
-#Сжатие нескольких файлов / папки
+# Сжатие нескольких файлов / папки
+# Путь файла вносится через код
 import os
 import zipfile
 
-fantasi_zip = zipfile.ZipFile('C:\\Users\\1\\Desktop\\Веб-дизайн.zip', 'w')
+fantasi_zip = zipfile.ZipFile('C:\\...\\Desktop\\file.zip', 'w')
 
-for folder, subfolders, files in os.walk('C:\\Users\\1\\Desktop\\Веб-дизайн'):
+for folder, subfolders, files in os.walk('C:\\...\\Desktop\\file'):
 	for file in files:
-		fantasi_zip.write(os.path.join(folder, file), os.path.relpath(os.path.join(folder, file), 'C:\\Users\\1\\Desktop\\Веб-дизайн'), compress_type = zipfile.ZIP_DEFLATED)
+		fantasi_zip.write(os.path.join(folder, file), os.path.relpath(os.path.join(folder, file), 'C:\\...\\Desktop\\file'), compress_type = zipfile.ZIP_DEFLATED)
 
 fantasi_zip.close()
