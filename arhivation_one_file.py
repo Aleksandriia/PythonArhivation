b35a1f917ec1b0afb1a456932aee762aa3e1d55f
Alexandria <77101492+Aleksandriia@users.noreply.github.com>
2022-10-26 01:46:38 +0300

    Update file

--- arhivation_one_file.py
+++ arhivation_one_file.py
@@ -1,7 +1,9 @@
-#Сжфтие одного файла
+# Сжатие одного файла
+# Редактирование в коде
+
 import zipfile
 
-jungle_zip = zipfile.ZipFile('C:\\Users\\1\\Desktop\\Веб-дизайн.zip', 'w')
-jungle_zip.write('C:\\Users\\1\\Desktop\\Веб-дизайн', compress_type=zipfile.ZIP_DEFLATED)
+jungle_zip = zipfile.ZipFile('C:\\Users\\1\\Desktop\\file.zip', 'w')
+jungle_zip.write('C:\\Users\\1\\Desktop\\file', compress_type=zipfile.ZIP_DEFLATED)
 
 jungle_zip.close()
